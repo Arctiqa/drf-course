c0dfc6595a5e0aaba8e0e9e003b19e5389fa25c9
Arctiqa <Sinitro@yandex.ru>
2024-05-28 01:08:56 +0800

    init

---
+++ users/views.py
@@ -0,0 +1,9 @@
+from rest_framework import generics
+
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserListAPIView(generics.ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
