b5f1e76eb529d5cc18a278dba4b17eeb7cb353ea
Arctiqa <Sinitro@yandex.ru>
2024-06-03 00:40:28 +0800

    users

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import UserListAPIView
+from users.views import UserListAPIView, UserCreateAPIView, UserDetailAPIView, UserUpdateAPIView, UserDestroyAPIView
 
 app_name = UsersConfig.name
 
@@ -11,5 +11,9 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='token'),
     path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
 
-    path('', UserListAPIView.as_view(), name='users'),
+    path('', UserListAPIView.as_view(), name='users_list'),
+    path('create/', UserCreateAPIView.as_view(), name='users_create'),
+    path('<int:pk>/', UserDetailAPIView.as_view(), name='users_detail'),
+    path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='users_update'),
+    path('delete/<int:pk>/', UserDestroyAPIView.as_view(), name='users_delete'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
+from habits.permissions import IsOwner
 from users.models import User
 from users.serializers import UserSerializer
 
@@ -7,3 +9,32 @@ from users.serializers import UserSerializer
 class UserListAPIView(generics.ListAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        user.set_password(user.password)
+        user.save()
+
+
+class UserDetailAPIView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
